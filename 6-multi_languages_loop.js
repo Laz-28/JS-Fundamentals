bc88adf4a1cd38b9c9ba103a25d3a9ebdc55cb8b
Otieno Lazaro <otienolazaro111@gmail.com>
2025-09-13 18:57:14 +0300

    for of loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,6 @@
-let arr = ["C is fun","Python is cool", "JavaScript is amazing"];
-let i = 0;
-while(i < arr.length){
-    console.log(arr[i]);
-    i++;
+const lines = ["C is fun", "Python is cool", "JavaScript is amazing"];
+
+for (const line of lines) {
+  console.log(line);
 }
 
